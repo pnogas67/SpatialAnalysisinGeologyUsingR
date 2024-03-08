a438136c7b7b883841a57d434946b86067e26af1
Pedro Nogueira <pnogas@gmail.com>
2024-03-08 16:25:48 +0000

    Update install.R
    remove most of the packages

--- install.R
+++ install.R
@@ -1,17 +1,5 @@
 install.packages("readr")
-install.packages("sf")
-install.packages("terra")
-install.packages("stringr")
-install.packages("tidyr")
-install.packages("sp")
-install.packages("psych")
-install.packages("corrgram")
-install.packages("jsonlite")
-install.packages("osmdata")
-install.packages("ggplot2")
-install.packages("leaflet")
-install.packages("raster")
-install.packages("ggspatial")
+
 
 
 
